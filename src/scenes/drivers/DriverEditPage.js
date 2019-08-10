f2ada3e768dba3ec1e80f004f35dee99e48d72b2
Sarkhan Rasullu <sarkhanrasullu@gmail.com>
2019-08-10 20:09:42 +0400

    changes

--- src/scenes/drivers/DriverEditPage.js
+++ src/scenes/drivers/DriverEditPage.js
@@ -1,39 +1,84 @@
 import React, { Component } from 'react'
-import {TableColumn} from '../../components/react_multiplatform_components';
+import {InputField} from '../../components/react_multiplatform_components';
 import EntityEditPage from '../../components/entityeditpage/EntityEditPage';
+import { InputFieldType } from '../../components/react_multiplatform_components';
+import { SelectBox } from '../../components/react_multiplatform_components/src/datatable/DataTableTypes';
 
-const formDataFields = 
-    [
-        new TableColumn("userId.thumbnail","Image","imagepicker"),
-        new TableColumn(null,null,"empty"),
-        new TableColumn("userId.name","name"),
-        new TableColumn("userId.surname","surname"),
-        new TableColumn("userId.email","email"),
-        new TableColumn("userId.phone","phone"),
-        new TableColumn("userId.nationalityId.id","nationality"),
-        new TableColumn("userId.password", "password"),
-        new TableColumn("userId.roleId.id", "role"),
-        new TableColumn("age"),
-        new TableColumn("gender"),
-        new TableColumn("isVerified","Verified"),
-        new TableColumn("price1","Baku"),
-        new TableColumn("price2","Absheron"),
-        new TableColumn("price3","Out of Absheron"),
-        new TableColumn(null,null,"empty"),
-        new TableColumn("driverLicenseFront","License Front","imagepicker"),
-        new TableColumn("driverLicenseBack","License Back","imagepicker"),
-        new TableColumn("about",null,"textarea"),
-    ];
+const rows = [
+    {
+        items:[
+            new InputField("target.userId.thumbnail","Image",InputFieldType.IMAGE_URL),
+            null
+        ]
+    },
+    {
+        items:[
+            new InputField("target.userId.name","name"),
+            new InputField("target.userId.surname","surname"),
+        ]
+    },
+    {
+        items:[
+            new InputField("target.userId.email","email"),
+            new InputField("target.userId.phone","phone"),
+        ]
+    },
+    {
+        items:[
+            new SelectBox("target.userId.nationalityId.id","nationality","/api/nationalities","id","name"),
+            new InputField("target.userId.password", "password",InputFieldType.PASSWORD),
+        ]
+    },
+    {
+        items:[
+            new SelectBox("target.userId.roleId.id", "role", "/api/userRoles","id","name"),
+            new InputField("target.age","age"),
+        ]
+    },
+    {
+        items:[
+            new SelectBox("target.genderId.id","gender", "/api/genders","id","name"),
+            new InputField("target.isVerified","Verified", InputFieldType.CHECK_BOX),
+        ]
+    },
+    {
+        items:[
+            new InputField("target.price1","Baku"),
+            new InputField("target.price2","Absheron"),
+        ]
+    },
+    {
+        items:[
+            new InputField("target.price3","Out of Absheron"),
+            null
+        ]
+    },
+    {
+        items:[
+            new InputField("target.driverLicenseFront","License Front",InputFieldType.IMAGE_URL),
+            null
+        ]
+    },
+    {
+        items:[
+            new InputField("target.driverLicenseBack","License Back",InputFieldType.IMAGE_URL),
+            null
+        ]
+    },
+    {
+        items:[
+            new InputField("target.about",null,InputFieldType.TEXT_AREA),
+        ]
+    }
+]
 
 class DriverEditPage extends Component {
     render() {
         return (
                 <EntityEditPage
-                    endpoint="/admin/drivers"
-                    select_endpoint="/drivers"
-                    callback_url="/drivers"
-                    projection="driverProjection"
-                    formDataFields={formDataFields}
+                    endpoint_select="/api/drivers/{id}?projection=driverProjection"
+                    endpoint_add_or_save="/api/admin/drivers"
+                    formFields={rows}
                 />
         )
     }
